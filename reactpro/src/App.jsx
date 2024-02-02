54c50c30d94897b4e63122a249cd79551d46f695
Dmytro <dimalimaredmi@gmail.com>
2024-02-02 12:25:44 +0200

    6 lesson ended

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,13 +1,14 @@
-import Hello from "./components/Hello"
+import Hello from "./components/Hello";
 
-function App() { // Компонент, який створений за допомогою React
-// Увесь код знизу називається JSX. Саме цей код відповідає за те, що відображається на веб-сторінці.
+function App() {
+  // Компонент, який створений за допомогою React
+  // Увесь код знизу називається JSX. Саме цей код відповідає за те, що відображається на веб-сторінці.
   return (
-    <div className="App"> {/* Це div нашого компоненту.*/}
-        <Hello/> {/* Використання Hello, якого ми отримали з import. */}
-        
+    <div className="App">
+      {/* Це div нашого компоненту.*/}
+      <Hello /> {/* Використання Hello, якого ми отримали з import. */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- reactpro/src/components/Hello.jsx
+++ reactpro/src/components/Hello.jsx
@@ -1,4 +1,4 @@
-function Hello () {
-    return <h1>Hello from a component!</h1>
+function Hello() {
+  return <h1>Hello from a component!</h1>;
 }
 export default Hello;
