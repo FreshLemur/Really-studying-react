5233c1d19b3e20a47268d878e1721de6479fb47e
Dmytro <dimalimaredmi@gmail.com>
2024-02-02 11:15:15 +0200

    4 lesson before delete the code in app.jsx

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -2,30 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
+function App() { // Компонент, який створений за допомогою React
   const [count, setCount] = useState(0)
-
+// Увесь код знизу називається JSX. Саме цей код відповідає за те, що відображається на веб-сторінці.
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+    <div className="App"> {/* Це div нашого компоненту */}
+      <div> {/* Це div іконок */}
+        <a href="https://vitejs.dev" target="_blank"> {/* Посилання всередині vite */}
+          <img src="/vite.svg" className="logo" alt="Vite logo" /> {/* vite logo */}
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" target="_blank"> {/* Посилання всередині react */}
+          <img src={reactLogo} className="logo react" alt="React logo" /> {/* react logo */}
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Vite + React</h1> {/* Текст по центру */}
+      <div className="card"> {/* div з лічильником */}
+        <button onClick={() => setCount((count) => count + 1)}> {/* лічильник */}
+          count is {count} {/* число лічильника */}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR {/* code вказує на програмний код, текст всередині p */}
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more {/* p внизу. */}
       </p>
     </div>
   )
--- reactpro/src/main.jsx
+++ reactpro/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+ReactDOM.createRoot(document.getElementById('root')).render( // Достукалися до div в index.html за допомогою id, а потім використали метод render
+  <React.StrictMode> {/* Функціональний коментар. */}
+    <App /> {/* Його імпортовано на 3 рядку. Ctrl + LMB на нього, побачимо звідки він імпортується.*/}
   </React.StrictMode>,
 )
