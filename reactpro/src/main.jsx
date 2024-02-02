45ca758b92a8a120a7e78cdd57d314eb39949d49
Dmytro <dimalimaredmi@gmail.com>
2024-02-02 11:49:47 +0200

    4 lesson end

--- reactpro/src/main.jsx
+++ reactpro/src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render( // Достукалися до div в index.html за допомогою id, а потім використали метод render
   <React.StrictMode> {/* Функціональний коментар. */}
-    <App /> {/* Його імпортовано на 3 рядку. Ctrl + LMB на нього, побачимо звідки він імпортується.*/}
+    <App /> {/* Рендеримо реакт компонент. Його імпортовано на 3 рядку. Ctrl + LMB на нього, побачимо звідки він імпортується.*/}
   </React.StrictMode>,
 )
