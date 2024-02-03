de1e5c126bceb65f49372aa96698547029bfd0ca
Dmytro <dimalimaredmi@gmail.com>
2024-02-03 16:56:57 +0200

    11 lesson ended

--- reactpro/src/components/Hello.jsx
+++ reactpro/src/components/Hello.jsx
@@ -1,38 +1,11 @@
-// 3 приклад деструктуризації.
-function Hello({ message, name, emoji }) {
+function Hello(props) {
+  // props.name = "Some name"; Якщо ми спробуємо переприсвоїти значення пропсу, то це просто буде помилка, тому що це не можливо.
   return (
     <div>
       <h1>
-        {message} {name} {emoji}
+        {props.message} {props.name} {props.emoji}
       </h1>
     </div>
   );
 }
 export default Hello;
-
-// 1 приклад, не деструктуризований.
-// function Hello(props) {
-//   return (
-//     <div>
-//       <h1>
-//         {props.message} {props.name}
-//       </h1>
-//       {/* Достукування до властивості об'єкту. Виведе Hello, I`m Dmytro. */}
-//     </div>
-//   );
-// }
-// export default Hello;з
-// Можна ось так використовувати.
-
-// 2 приклад, вже деструктуризований, проте ще не найкраща деструктуризація.
-// function Hello(props) {
-//   const { message, name } = props;
-//   return (
-//     <div>
-//       <h1>
-//         {message} {name}
-//       </h1>
-//     </div>
-//   );
-// }
-// export default Hello;
