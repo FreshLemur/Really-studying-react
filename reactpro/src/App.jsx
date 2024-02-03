6bbb57406674154a6f3bdc7bfedc4499fb4c7d78
Dmytro <dimalimaredmi@gmail.com>
2024-02-03 16:32:06 +0200

    9 lesson ended

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -2,12 +2,11 @@ import Hello from "./components/Hello";
 function App() {
   return (
     <div className="App">
-      <Hello />
-      <Hello />
-      <Hello />
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Dmytro" message="Hello, I`m" />
+      <Hello name="Vitaly" message="Hi, my name is" />
+      <Hello name="Kolya" message="Fuck u, " />
+      {/* name і message це Пропс, який ми передаємо в компонент Hello, це буде об'єкт. 
+      Ми створили кілька компонентів, яким передаємо значення, отже це динамічні компоненти.*/}
     </div>
   );
 }
--- reactpro/src/components/Hello.jsx
+++ reactpro/src/components/Hello.jsx
@@ -1,11 +1,23 @@
-function Hello() {
+function Hello(props) {
+  console.log(props); // props це об'єкт.
   return (
     <div>
-      <h1>Hello Mike</h1>
+      <h1>
+        {props.message} {props.name}
+      </h1>
+      {/* Достукування до властивості об'єкту. Виведе Hello, I`m Dmytro. */}
     </div>
   );
 }
 export default Hello;
 
-/*
- */
+// function Hello(props) {
+//   console.log(props); // props це об'єкт.
+//   return (
+//     <div>
+//       <h1>Hello {props.name}</h1> {/* Достукування до властивості об'єкту. */}
+//       </div>
+//       );
+//     }
+//     export default Hello;
+// Це те, як можна зробити, цей компонент вже є динамічним, проте не повністю.
