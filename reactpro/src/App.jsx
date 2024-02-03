2bdf6be7e4167715abe5d5e8261897314244164d
Dmytro <dimalimaredmi@gmail.com>
2024-02-03 16:09:46 +0200

    8 lesson ended

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,12 +1,13 @@
 import Hello from "./components/Hello";
-
 function App() {
-  // Компонент, який створений за допомогою React
-  // Увесь код знизу називається JSX. Саме цей код відповідає за те, що відображається на веб-сторінці.
   return (
     <div className="App">
-      {/* Це div нашого компоненту.*/}
-      <Hello /> {/* Використання Hello, якого ми отримали з import. */}
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   );
 }
--- reactpro/src/main.jsx
+++ reactpro/src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render( // Достукалися до div в index.html за допомогою id, а потім використали метод render
-  <React.StrictMode> {/* Функціональний коментар. */}
-    <App /> {/* Рендеримо реакт компонент. Його імпортовано на 3 рядку. Ctrl + LMB на нього, побачимо звідки він імпортується.*/}
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
