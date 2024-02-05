2496d414ab9bf1d7bce680645018363c803b6189
Dmytro <dimalimaredmi@gmail.com>
2024-02-05 09:10:07 +0200

    12 lesson ended

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,10 +1,36 @@
 import Hello from "./components/Hello";
 function App() {
+  const person = {
+    name: "Dmytro",
+    message: "Hello, I`m",
+    emoji: "😊",
+    seatNumbers: [1, 4, 7],
+  };
   return (
     <div className="App">
-      <Hello name="Dmytro" message="Hello, I`m" emoji="😊" />
+      <Hello person={person} />
+      {/* Передача об'єкту в компонент. */}
     </div>
   );
 }
 
 export default App;
+
+// import Hello from "./components/Hello";
+// function App() {
+//   const seatNumbers = [1, 4, 7];
+//   return (
+//     <div className="App">
+//       <Hello
+//         name="Dmytro"
+//         message="Hello, I`m"
+//         emoji="😊"
+//         seatNumbers={seatNumbers}
+//         // Передача масиву в компонент.
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+// Це передача масиву в компонент, seatNumbers={seatNumbers}.
