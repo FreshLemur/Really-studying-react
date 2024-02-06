38d8ca68847414bf1b2692eaea2843361ad8e41e
Dmytro <dimalimaredmi@gmail.com>
2024-02-06 19:12:25 +0200

    lesson 17.5 ended

--- reactpro/src/components/Conditional.jsx
+++ reactpro/src/components/Conditional.jsx
@@ -1,19 +1,29 @@
-import Code from "./Code";
-import Welcome from "./Welcome";
-
 export default function Conditional() {
   const display = true;
-  const messageOne = <h1>Text 1</h1>;
-  const messageTwo = <h1>Text 2</h1>;
   let message;
   if (display) {
-    message = messageOne;
+    message = <h1>Text from if true</h1>;
   } else {
-    message = messageTwo;
+    message = <h1>Text from else false</h1>;
   }
   return message;
 }
+// Цей варіант є ще кращий, тому що ми прописуємо набагато менше коду і маємо той самий 1 return.
+
+// export default function Conditional() {
+//   const display = true;
+//   const messageOne = "Text if";
+//   const messageTwo = "Text else";
+//   let message;
+//   if (display) {
+//     message = messageOne;
+//   } else {
+//     message = messageTwo;
+//   };
+//   return message;
+// };
 // А ось цей варіант прекрасно виконує те, що ми хотіли зробити. Тепер ми використовуємо лише 1 return.
+// Але можна все зробити ще краще!
 
 // export default function Conditional() {
 //     const display = false;
