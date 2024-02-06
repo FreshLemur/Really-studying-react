809a39402f1a78a3435751d9e4d5dcdb5fd4071a
Dmytro <dimalimaredmi@gmail.com>
2024-02-06 19:49:09 +0200

    18 lesson ended

--- reactpro/src/components/Conditional.jsx
+++ reactpro/src/components/Conditional.jsx
@@ -1,38 +1,27 @@
+import Code from "./Code";
+import Welcome from "./Welcome";
+
 export default function Conditional() {
-  const display = true;
-  let message;
-  if (display) {
-    message = <h1>Text from if true</h1>;
-  } else {
-    message = <h1>Text from else false</h1>;
-  }
-  return message;
+  let isDisplay = false;
+  return isDisplay ? <Welcome /> : <Code />;
 }
-// Цей варіант є ще кращий, тому що ми прописуємо набагато менше коду і маємо той самий 1 return.
+// Вже в цьому випадку ми за допомогою тернарних операторів умовно рендеримо компоненти.
+// Якщо isDisplay true, то відрендериться компонент Welcome, а якщо isDisplay = false, то
+// відрендериться компонент Code.
 
 // export default function Conditional() {
-//   const display = true;
-//   const messageOne = "Text if";
-//   const messageTwo = "Text else";
-//   let message;
-//   if (display) {
-//     message = messageOne;
-//   } else {
-//     message = messageTwo;
-//   };
+//   let isDisplay = false;
+//   return isDisplay ? <h1>Message from true</h1> : <h1>Message from false</h1>;
+// }
+// Ось вже чудовий, прекрасно працюючий приклад. В верхньому лише додадуться компоненти.
+
+// export default function Conditional() {
+//   const isDisplay = false;
+//   let message = isDisplay ? (
+//     <h1>Text from if true</h1>
+//   ) : (
+//     <h1>Text from else false</h1>
+//   );
 //   return message;
-// };
-// А ось цей варіант прекрасно виконує те, що ми хотіли зробити. Тепер ми використовуємо лише 1 return.
-// Але можна все зробити ще краще!
-
-// export default function Conditional() {
-//     const display = false;
-//     const messageOne = <h1>Text 1</h1>;
-//     const messageTwo = <h1>Text 2</h1>;
-//     if (display) {
-//       return messageOne;
-//     } else {
-//       return messageTwo;
-//     }
-//   }
-// Цей варіант вже дуже схожий на те, чого ми хочемо добитися. Проте ми досі використовуємо 2 return.
+// }
+// Ось вже приклад використання тернарних операторів. Проте можна зробити ще краще! ⬆⬆⬆
